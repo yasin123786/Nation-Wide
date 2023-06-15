32d6016b8297668af0081ceb7dfcb3f239eec7de
Rehan-Bhatti-git <rehanwhynot123@gmail.com>
2023-06-15 15:43:22 +0500

    new-one

---
+++ js/index.js
@@ -0,0 +1,92 @@
+          function changeText_1()
+        {
+            var id=document.getElementsByName("button-1")[0].id;
+            {
+                document.getElementById("text-1").style.display="block";
+                document.getElementsByName("button-1")[0].id=1;
+            }
+            var id=document.getElementsByName("button-2")[0].id;
+            {
+                document.getElementById("text-2").style.display="none";
+                document.getElementsByName("button-2")[0].id=2;
+            }
+            var id=document.getElementsByName("button-3")[0].id;
+            {
+                document.getElementById("text-3").style.display="none";
+                document.getElementsByName("button-3")[0].id=3;
+            }
+            var id=document.getElementsByName("button-4")[0].id;
+            {
+                document.getElementById("text-4").style.display="none";
+                document.getElementsByName("button-4")[0].id=4;
+            }
+        }
+        function changeText_2()
+        {
+          var id=document.getElementsByName("button-1")[0].id;
+            {
+                document.getElementById("text-1").style.display="none";
+                document.getElementsByName("button-1")[0].id=1;
+            }
+            var id=document.getElementsByName("button-2")[0].id;
+            {
+                document.getElementById("text-2").style.display="block";
+                document.getElementsByName("button-2")[0].id=2;
+            }
+            var id=document.getElementsByName("button-3")[0].id;
+            {
+                document.getElementById("text-3").style.display="none";
+                document.getElementsByName("button-3")[0].id=3;
+            }
+            var id=document.getElementsByName("button-4")[0].id;
+            {
+                document.getElementById("text-4").style.display="none";
+                document.getElementsByName("button-4")[0].id=4;
+            }
+        }
+        function changeText_3()
+        {
+          var id=document.getElementsByName("button-1")[0].id;
+            {
+                document.getElementById("text-1").style.display="none";
+                document.getElementsByName("button-1")[0].id=1;
+            }
+            var id=document.getElementsByName("button-2")[0].id;
+            {
+                document.getElementById("text-2").style.display="none";
+                document.getElementsByName("button-2")[0].id=2;
+            }
+            var id=document.getElementsByName("button-3")[0].id;
+            {
+                document.getElementById("text-3").style.display="block";
+                document.getElementsByName("button-3")[0].id=3;
+            }
+            var id=document.getElementsByName("button-4")[0].id;
+            {
+                document.getElementById("text-4").style.display="none";
+                document.getElementsByName("button-4")[0].id=4;
+            }
+        }
+        function changeText_4()
+        {
+          var id=document.getElementsByName("button-1")[0].id;
+            {
+                document.getElementById("text-1").style.display="none";
+                document.getElementsByName("button-1")[0].id=1;
+            }
+            var id=document.getElementsByName("button-2")[0].id;
+            {
+                document.getElementById("text-2").style.display="none";
+                document.getElementsByName("button-2")[0].id=2;
+            }
+            var id=document.getElementsByName("button-3")[0].id;
+            {
+                document.getElementById("text-3").style.display="none";
+                document.getElementsByName("button-3")[0].id=3;
+            }
+            var id=document.getElementsByName("button-4")[0].id;
+            {
+                document.getElementById("text-4").style.display="block";
+                document.getElementsByName("button-4")[0].id=4;
+            }
+        }
